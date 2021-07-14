92a852873baf60037b87bca2af59e2c4bb787bb0
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2021-07-14 12:20:46 +0900

    backup

--- _v6_proc_pointer.py
+++ _v6_proc_pointer.py
@@ -24,6 +24,22 @@ import subprocess
 import pyautogui
 import random
 
+import numpy as np
+import cv2
+
+from PIL import Image, ImageDraw, ImageFont
+import unicodedata
+import mojimoji
+
+
+
+# フォント
+qPath_fonts   = '_fonts/'
+qFont_default = {'file':qPath_fonts + '_vision_font_ipaexg.ttf','offset':8}
+qFont_status  = {'file':qPath_fonts + '_vision_font_ipag.ttf','offset':8}
+qFont_zh = {'file':'C:/Windows/Fonts/msyh.ttc', 'offset':5}
+qFont_ko = {'file':'C:/Windows/Fonts/batang.ttc', 'offset':10}
+
 
 
 # 共通ルーチン
@@ -33,6 +49,9 @@ import  _v6__qFunc
 qFunc = _v6__qFunc.qFunc_class()
 import  _v6__qLog
 qLog  = _v6__qLog.qLog_class()
+
+import _v6__qGuide
+qGuide= _v6__qGuide.qGuide_class()
 
 qPLATFORM        = qRiKi.getValue('qPLATFORM'        )
 qRUNATTR         = qRiKi.getValue('qRUNATTR'         )
@@ -124,7 +143,84 @@ if (res == False):
     dic['dayEnd']       = '16:05:00'
     dic['lunchStart']   = '12:05:00'
     dic['lunchEnd']     = '12:55:00'
+    dic['signDisplay']  = 'yes'
+    dic['signSound']    = 'yes'
     res = qRiKi_key.putCryptJson(config_file=config_file, put_dic=dic, )
+
+
+
+def in_japanese(txt=''):
+        t = txt.replace('\r', '')
+        t = t.replace('\n', '')
+        try:
+            for s in t:
+                name = unicodedata.name(s) 
+                if ('CJK UNIFIED' in name) \
+                or ('HIRAGANA' in name) \
+                or ('KATAKANA' in name):
+                    return True
+        except Exception as e:
+            pass
+        return False
+
+
+
+def txt2img(txts=[], bg_color=(0,0,0), txt_color=(255,255,255), ):
+
+    # フォント定義
+    font128_default  = ImageFont.truetype(qFont_default['file'], 128, encoding='unic')
+    font128_defaulty =                    qFont_default['offset']
+
+    # キャンバス用意
+    maxlen = 0
+    for i in range(0, len(txts)):
+        if (in_japanese(txts[i]) == True):
+            lenstr = len(txts[i]) * 2
+        else:
+            lenstr = len(txts[i])
+        if (maxlen < lenstr):
+            maxlen = lenstr
+
+    draw_height = int(10 + (128 + 10) * len(txts))
+    draw_width = int(128 * (maxlen + 1))
+    #bg_color = (0,0,0)
+    text_img  = Image.new('RGB', (draw_width, draw_height), bg_color)
+    text_draw = ImageDraw.Draw(text_img)
+
+    #print(draw_width, draw_height)
+
+    # 文字描写
+    #txt_color = (255,255,255)
+    for i in range(0, len(txts)):
+        text_draw.text((128/2, (128 + 10)*i + font128_defaulty), txts[i], font=font128_default, fill=txt_color)
+
+        #print(txts[i])
+
+    return np.asarray(text_img)
+
+
+def imgOverlay(img1_base, img2_overlay, threshold=64, ):
+
+    # ベースサイズ取得
+    img1_height, img1_width = img1_base.shape[:2]
+
+    # リサイズ
+    img2_resize = cv2.resize(img2_overlay, (img1_width, img1_height))
+
+    # 重ね合わせ領域
+    img2_gray    = cv2.cvtColor(img2_resize, cv2.COLOR_BGR2GRAY)
+    ret, img2_bw = cv2.threshold(img2_gray, threshold, 255, cv2.THRESH_BINARY)
+    img2_invert  = cv2.bitwise_not(img2_bw)
+
+    # 合成
+    img1 = cv2.bitwise_and(img1_base,   img1_base,   mask=img2_invert)
+    img2 = cv2.bitwise_and(img2_resize, img2_resize, mask=img2_bw)
+    img_out = cv2.add(img1, img2)
+
+    #cv2.cvCopy(img_txts, img_base, img_bw)
+    #cv2.cvCopy(img_txts, img_base, img_bw)
+
+    return img_out
 
 
 
@@ -155,6 +251,7 @@ class proc_pointer:
         self.proc_last = None
         self.proc_step = '0'
         self.proc_seq  = 0
+        self.lastSign  = '00:00'
 
         json_file = '_v6_proc_pointer_key.json'
         self.mouseEnable  = 'yes'
@@ -166,6 +263,8 @@ class proc_pointer:
         self.dayEnd       = '16:05:00'
         self.lunchStart   = '12:05:00'
         self.lunchEnd     = '12:55:00'
+        self.signDisplay  = 'yes'
+        self.signSound    = 'yes'
         res, json_dic = qRiKi_key.getCryptJson(config_file=json_file, auto_crypt=False, )
         if (res == True):
             self.mouseEnable  = dic['mouseEnable']
@@ -177,6 +276,8 @@ class proc_pointer:
             self.dayEnd       = dic['dayEnd']
             self.lunchStart   = dic['lunchStart']
             self.lunchEnd     = dic['lunchEnd']
+            self.signDisplay  = dic['signDisplay']
+            self.signSound    = dic['signSound']
         qLog.log('info', self.proc_id, 'mouseEnable  = ' + str(self.mouseEnable ), display=self.logDisp, )
         qLog.log('info', self.proc_id, 'mousePointer = ' + str(self.mousePointer), display=self.logDisp, )
         qLog.log('info', self.proc_id, 'changeScreen = ' + str(self.changeScreen), display=self.logDisp, )
@@ -186,6 +287,8 @@ class proc_pointer:
         qLog.log('info', self.proc_id, 'dayEnd       = ' + str(self.dayEnd      ), display=self.logDisp, )
         qLog.log('info', self.proc_id, 'lunchStart   = ' + str(self.lunchStart  ), display=self.logDisp, )
         qLog.log('info', self.proc_id, 'lunchEnd     = ' + str(self.lunchEnd    ), display=self.logDisp, )
+        qLog.log('info', self.proc_id, 'signDisplay  = ' + str(self.signDisplay ), display=self.logDisp, )
+        qLog.log('info', self.proc_id, 'signSound    = ' + str(self.signSound   ), display=self.logDisp, )
 
         try:
             (w, h) = pyautogui.size()
@@ -402,6 +505,7 @@ class proc_pointer:
     def check(self, ):
         nowTime = datetime.datetime.now()
         nowHHMMSS = nowTime.strftime('%H:%M:%S')
+        nowHHMM   = nowTime.strftime('%H:%M')
         nowYOUBI  = nowTime.strftime('%a')
         if (nowHHMMSS < self.dayStart) \
         or (nowHHMMSS > self.dayEnd):
@@ -426,6 +530,30 @@ class proc_pointer:
                 qFunc.statusSet(self.fileBsy, False)
                 qLog.log('info', self.proc_id, 'mouse pointer stop (YOUBI=' & nowYOUBI & ')', display=self.logDisp, )
             return
+
+        # Sign 00:00
+        if (nowHHMM[-3:] == ':00'):
+            if (nowHHMM != self.lastSign):
+                self.lastSign = nowHHMM
+                #print(nowHHMM)
+                qLog.log('info', self.proc_id, nowHHMM, display=self.logDisp, )
+
+                # ベース画像
+                img_base = cv2.imread('_icons/RiKi_base.png')
+
+                # 文字画像
+                zen = mojimoji.han_to_zen(nowHHMM)
+                img_overlay = txt2img([zen], txt_color=(255,0,255) )
+
+                # 合成
+                image = imgOverlay(img_base, img_overlay)
+
+                # 表示
+                qGuide.init(panel='5', title='', image=image, alpha_channel=0.3, )
+                qGuide.open()
+                qGuide.setMessage(txt='', )
+                time.sleep(3.00)
+                qGuide.close()
 
         (x, y) = pyautogui.position()
         if (x != self.last_x) or (y != self.last_y):
